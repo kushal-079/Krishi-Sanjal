2977a5c0699ea2d7a4db9e86c1c3c8d29c49b89e
kushal-079 <kushalsapkota.0111@gmail.com>
2024-11-16 20:34:39 +0545

    Hero and middle section update

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,12 +2,17 @@ import React from 'react'
 import Header1 from './Components/Header1/Header1'
 import Navbar from './Components/Navbar/Navbar'
 import Footer1 from './Components/Footer1/Footer1'
+import Hero1 from './Components/Content1/Hero1/Hero1'
+import Middle1 from './Components/Content1/Middle1/Middle1'
+
 
 const App = () => {
   return (
     <div className='App'>
       <Header1/>
       <Navbar/>
+      <Hero1/>
+      <Middle1/>
       <Footer1/>
     </div>
   )
--- Frontend/src/assets/assets.jsx
+++ Frontend/src/assets/assets.jsx
@@ -1,5 +1,13 @@
 import logo from './Krisi.png';
+import hero from './HERO.png';
+import inventory from './Inventory.jpg';
+import order from './Order.jpg';
+import setting from './Setting.png';
 
 export const assets = {
     logo,
+    hero,
+    inventory,
+    order,
+    setting
 };
