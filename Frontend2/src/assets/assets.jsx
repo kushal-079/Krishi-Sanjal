853387eb6ac1978f3d6c3f8c763c8e586ab04cd8
kushal-079 <kushalsapkota.0111@gmail.com>
2024-12-09 21:21:32 +0545

    Frontend2

---
+++ Frontend2/src/assets/assets.jsx
@@ -0,0 +1,15 @@
+import logo from './Krisi.png';
+import hero from './HERO.png';
+import buy from './Buy.jpg';
+import cart from './Cart.jpg';
+import profile from './Profile.jpg';
+import cart111 from './Cart111.png';
+
+export const assets = {
+    logo,
+    hero,
+    buy,
+    cart,
+    profile,
+    cart111,
+};
